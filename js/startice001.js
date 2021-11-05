f750dae20e4db7ac96d931a3bf51a37fbfb20c77
ejuin12 <91935287+ejuin12@users.noreply.github.com>
2021-11-05 14:21:32 +0900

    Add files via upload

---
+++ js/startice001.js
@@ -0,0 +1,58 @@
+$(function(){
+    //----------------------------------------------------
+
+
+
+    $('#top_banner i').on('click',function(){
+        $(this).parent().parent().slideUp();
+    });
+
+
+
+
+    $('.main_visual').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:4000,
+        pauseOnHover:false,
+    });
+    $('.main_visual figure').eq(1).addClass('on');
+
+    $('.main_visual').on('afterChange',function(e,s,c){
+        $('.main_visual figure').eq(c+1).addClass('on').siblings().removeClass('on');
+    });
+
+
+
+
+    $('.pr_slider').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:2000,
+        slidesToShow:3,
+    });
+    $('#con01 .btn i.xi-angle-left').on('click',function(){
+        $('.pr_slider').slick('slickPrev');
+    });
+    $('#con01 .btn i.xi-angle-right').on('click',function(){
+        $('.pr_slider').slick('slickNext');
+    });
+
+
+
+
+    $(window).on('scroll',function(){
+        var sct=$(window).scrollTop();
+          
+        $('#right_pop').css({top:120+sct});
+    });
+
+
+
+
+
+    
+    
+    
+    //----------------------------------------------------
+});
