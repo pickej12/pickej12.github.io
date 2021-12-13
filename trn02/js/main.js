c154c5e3b1bc21c3c754e2ae46c87664c5cbd652
ejuin12 <91935287+ejuin12@users.noreply.github.com>
2021-12-13 14:15:37 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+//------------------------------------------------------------------------------------
+    $('#main').fullpage({
+        anchors:['c01','c02','c03','c04','c05','footer'],
+        // navigation:true,
+        afterLoad:function(origin, destination, direction){
+            let txt = $('.navbar>li').eq(destination.index).find('a').text();
+            $('.section').eq(destination.index).addClass("on").siblings().removeClass('on');
+            $('.navbar>li').eq(destination.index).addClass("on").siblings().removeClass('on');
+            $('.now').text(txt);
+            destination.index===0 ? $('.header').fadeIn() : $('.header').fadeOut();
+            destination.index===0 ? $('.wheel').fadeIn() : $('.wheel').fadeOut();
+        },
+    });
+
+    // function cook (재료1, 재료2){
+    //     let 삶은라면=재료1+ "과" + 재료2 + "로 삶은라면";
+    //     return 삶은라면;
+    // }
+
+    // let 라면 = cook("신라면","스프");
+    // console.log(라면);
+
+
+//------------------------------------------------------------------------------------
+});
