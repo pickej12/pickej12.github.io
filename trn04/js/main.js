310f44b179c8664ce227bde464db5091bdc980e3
ejuin12 <91935287+ejuin12@users.noreply.github.com>
2021-12-31 17:57:00 +0900

    Add files via upload

--- trn04/js/main.js
+++ trn04/js/main.js
@@ -1,11 +1,26 @@
 $(function(){
 //---------------------------------------------------------------------------
+$('#popUp').draggable();
+
+$('#popUp a').on('click',function(){
+    $(this).parent().fadeOut();
+    return false;
+});
+
+$('.search').on('click', function(){
+    $('.search_form').stop().slideToggle();
+});
+
 
 $(window).on('scroll', ()=>{
     let sct=$(window).scrollTop();
+    console.log(sTop,sct);
     sct > 0 ? $('#header').addClass('on') : $('#header').removeClass('on');
     // if(sct > 0) {$('#header').addClass('on')}else{$('#header').removeClass('on')}
+    sct > 300 ? $('#toTop').fadeIn() : $('#toTop').fadeOut();
+    sct > sTop ? $('#solution').addClass('on') : $('#solution').removeClass('on');
 });
+
 
 $('.main_slider').slick({
     autoplay:true,
@@ -37,6 +52,22 @@ $('.pr_slider figure').eq(4).addClass('on');
 $('.pr_slider').on('afterChange',function(e,s,c){
     $('.pr_slider figure').eq(c+4).addClass('on').siblings().removeClass('on');
 });
+
+
+
+$('.familyLink .link>a').on('click', function(e){
+    // e.preventDefault();
+    $(this).prev().stop().slideToggle();
+    $(this).find('i').toggleClass('on');
+    return false;
+});
+
+$('#toTop a').on('click', e=> {
+    e.preventDefault();
+    $('html,body').animate({scrollTop:0},1000)
+});
+
+let sTop = $('#solution').offset().top - 200;
 
 //---------------------------------------------------------------------------
 });
