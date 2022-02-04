28dd204c4ac874f83e6872513f36873bcaad9636
ejuin12 <91935287+ejuin12@users.noreply.github.com>
2022-02-04 15:16:57 +0900

    Add files via upload

---
+++ 2022pf_ej/2022pf_ej/js/portfolio.js
@@ -0,0 +1,73 @@
+$(function(){
+    //-----------------------------------------------------
+    
+    let sc=$('.section');
+    let sideBar=$('nav li');
+    $('#main').fullpage({
+        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'training', 'profile'],
+        afterLoad:function(origin, destination, direction){
+            let idx=destination.index;
+            sc.eq(idx).addClass('on').siblings().removeClass('on');
+            sideBar.eq(idx).addClass('on').siblings().removeClass('on');
+        },
+    });
+
+
+
+    $(window).on('scroll', function(){
+        let sct=$(window).scrollTop();
+        $('.ani').each(function(){
+            let secTop = $(this).offset().top;
+            sct > secTop - 400 ? $(this).addClass('on') : $(this).removeClass('on'); 
+        });
+    });
+
+
+
+    var cursor = document.getElementById('cursor');
+    document.addEventListener('mousemove',function(e){
+        var x = e.clientX; 
+        var y = e.clientY;
+        cursor.style.left = e.pageX + "px";
+        cursor.style.top = e.pageY + "px";
+    });
+    
+
+
+    $('.mopen').on('click', function(){  
+        $(this).toggleClass('is-active');
+        $('#cover').slideToggle();
+    });
+
+
+
+    let cloneMenu = $('nav>ul').clone();
+    $('#cover').append(cloneMenu);
+
+
+    $('#cover a').on('click', function(){  
+        $('.mopen').toggleClass('is-active');
+        $('#cover').slideUp();
+    });
+
+    $('#cove a').on('click', function(){  
+        $('#cover').slideUp();
+    });
+
+    $('#cover').on('scroll wheel touchmove', function(){
+        return false;
+    });
+
+
+
+    //typed
+    new Typed('.slogan_txt',{
+        strings: ['기본을 중시하는 단단한 구현'],
+        typeSpeed:160,
+        cursorChar:"",
+        loop:true,
+        backSpeed:40,
+    });
+
+    //-----------------------------------------------------
+});
