c237b6f0c75d7628d7d3974d63247579942fe997
DESKTOP-M91CSUC\admin <ejuin12@gmail.com>
2024-05-17 04:47:21 +0900

    index- botanic 목업 이미지 변경, pf 순서변경

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,7 +5,7 @@ $(function(){
     let sideBar=$('nav li');
     $('#main').fullpage({
         
-        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'design_work', 'training', 'profile'],
+        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'design_work', 'training', 'profile'],
         afterLoad:function(origin, destination, direction){
             let idx=destination.index;
             sc.eq(idx).addClass('on').siblings().removeClass('on');
