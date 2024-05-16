18aa5e69a01fe77fe06dd9863727ab02de3de294
DESKTOP-M91CSUC\admin <ejuin12@gmail.com>
2024-05-17 03:50:04 +0900

    Botanic- 디자인 변경, 반응형 적용

--- pf04/js/botanic01.js
+++ pf04/js/botanic01.js
@@ -3,6 +3,32 @@ $(function(){
         startEvent: 'DOMContentLoaded',
     });
 
+    //popup
+    // const $popup = $('.popup');
+    // const hour = 24; // 24시간 동안 팝업 숨기기
+
+    // // 닫기 버튼 클릭 시 팝업 숨기기와 쿠키 설정
+    // $popup.on('click', '.close', function(e) {
+    //     e.preventDefault();
+    //     const hidePopup = $('.today_chk').prop('checked'); // 팝업을 숨길지 여부
+    //     if (hidePopup) {
+    //         const d = new Date();
+    //         d.setTime(d.getTime() + (hour * 60 * 60 * 1000));
+    //         const expires = `expires=${d.toUTCString()}`;
+    //         document.cookie = `hidePopup=true; ${expires}; path=/`; // 쿠키 설정
+    //     }
+    //     $popup.hide();
+    // });
+
+    // // 쿠키 확인하여 팝업을 보여줄지 결정
+    // if (document.cookie.indexOf('hidePopup=true') >= 0) {
+    //     $popup.hide(); // 팝업 숨기기
+    // } else {
+    //     $popup.show(); // 쿠키가 없으면 팝업을 보여줌
+    // }
+
+
+
     $('#con01 .slider').slick({
         arrows:false,
         autoplay:true,
@@ -19,7 +45,6 @@ $(function(){
 
 
 
-
    $('#con02 .container figure').slick({
        arrows:false,
        asNavFor: '#con02 .container figure',
@@ -27,7 +52,6 @@ $(function(){
        autoplaySpeed:5000,
        pauseOnHover:false,
    });
-
 
    $('#con02 .slider').on("afterChange", function(e,s,c){
 
@@ -42,26 +66,17 @@ $(function(){
     });
 
 
-    //con03 ani 적용하기
-    // $(window).on('scroll', function(){
-    //     let sct=$(window).scrollTop();
-    //     $('.ani').each(function(){
-    //         let secTop = $(this).offset().top;
-    //         sct > secTop - 400 ? $(this).addClass('on') : $(this).removeClass('on'); 
-    //     });
-    // });
-
     
     $('#con04 .poster').slick({
         arrows:false,
         autoplay:true,
-        autoplaySpeed:4000,
+        autoplaySpeed:3000,
         pauseOnHover:false,
-        slidesToShow:3,
+        slidesToShow:2,
         dots:true,
         responsive: [
             {
-              breakpoint: 769,
+              breakpoint: 768,
               settings: {
                 slidesToShow:1,
               }
@@ -69,21 +84,48 @@ $(function(){
           ]
     });
 
-    $('#con04 .btn i.xi-arrow-right').on('click', function(){
+    $('#con04 .btn .xi-angle-left').on('click', function(){
+        $('#con04 .poster').slick('slickPrev')
+    });
+    $('#con04 .btn .xi-angle-right').on('click', function(){
         $('#con04 .poster').slick('slickNext')
     });
 
 
 
+    $('#video01').YTPlayer({
+        videoURL:'https://youtu.be/xjxBR0uwtWc',
+        containment:'self',
+        autoPlay:true,
+        mute:true,
+        startAt:0,
+        opacity:1,
+        showControls:false,
+        playOnlyIfVisible:true,
+        optimizeDisplay:false,
+    });
+    
+    $('.park_video').on('click', function(){
+        $('#video01').YTPFullscreen();
+    });
+
+
+
+    $('#right_pop .xi-plus').on('click',function(){
+        $('.action-list').toggleClass('on');
+        $(this).toggleClass('on')
+    });
+
+
+    
     $('#toTop').on('click',function(){
         $('html, body').animate({scrollTop:0},500)
     });
     
     $(window).on('scroll',function(){
         var sct=$(window).scrollTop();
-        sct>1400 ? $('#toTop').fadeIn() : $('#toTop').fadeOut();
+        sct>1000 ? $('#toTop').fadeIn() : $('#toTop').fadeOut();
     });
-
 
 
 
@@ -91,9 +133,9 @@ $(function(){
         $('#header .bottom img').toggleClass('on');
         $('#header .top').toggleClass('on');
         $('.gnb').toggleClass('on');
+        $('.right').toggleClass('on');
         $(this).toggleClass('on');
     });
-
 
 
 
@@ -111,7 +153,7 @@ $(function(){
 	};
 
 	$(window).on('load resize', function() {
-		if($(window).width() > 768) {
+		if($(window).width() > 1024) {
 			Rsd.slick('unslick');
 		}else{
 			Rsd.not('.slick-initialized').slick(slickOptions);
