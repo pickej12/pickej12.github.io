642d14ff44c43c8dafcc2f239d99eebeb330e360
pickej12 <91935287+pickej12@users.noreply.github.com>
2025-02-17 15:16:29 +0900

    Update botanic01.js
    ios 비디오 재생시 fullscreen 재생 방지

--- pf04/js/botanic01.js
+++ pf04/js/botanic01.js
@@ -94,7 +94,7 @@ $(function(){
 
 
     $('#video01').YTPlayer({
-        videoURL:'https://youtu.be/xjxBR0uwtWc',
+        videoURL:'https://youtu.be/xjxBR0uwtWc?playsinline=1',
         containment:'self',
         autoPlay:true,
         mute:true,
